9b247acff2b0722bb0e9a69245ae48795e14aa55
Rafael <rafael.cusco14@gmail.com>
2026-08-24 23:43:27 -0300

    Add age comparison functionality

---
+++ 03_comparar_idades.py
@@ -0,0 +1,9 @@
+n1 = int(input("Digite a idade da primeira pessoa: "))
+n2 = int(input("Digite a idade da segunda pessoa: "))
+
+if n1 > n2:
+    print("a primeira idade é maior")
+elif n2 > n1:
+    print("a segunda idade é maior")
+else:
+    print("as idades são iguais")
