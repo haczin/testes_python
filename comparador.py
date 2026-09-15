c091b0eea1195d3cb03f73387b4db0d6d95d44e9
Rafael <rafael.cusco14@gmail.com>
2026-09-15 16:05:31 -0300

    Update and rename comparador_py to comparador.py

---
+++ comparador.py
@@ -0,0 +1,6 @@
+sam_idade = 16
+muito_novo = sam_idade < 17
+motorista_carro = sam_idade >= 17
+
+print(f"Sam não é muito novo para dirigir?  {muito_novo}")
+print(f"Sam pode dirigir! {motorista_carro}")
